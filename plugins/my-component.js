78a3dc710641c06ceff0948ea4b37480865f3e8c
pimlie <pimlie@hotmail.com>
2018-10-18 12:18:17 +0200

    add build/start script

--- plugins/my-component.js
+++ plugins/my-component.js
@@ -2,4 +2,9 @@ import Vue from 'vue'
 
 Vue.component('my-component', {
   template: `<div><h2>Hello My Component</h2></div>`
+  /*render: function(h) {
+    return h('div', null, [
+      h('h2', null, 'Hello My Component')
+    ])
+  }*/
 })
